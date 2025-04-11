fecfdfd331bd363bd04f051f3a7b197d603c6914
lorenypsum <loren.ypsum@gmail.com>
2025-04-11 00:17:21 -0300

    login e home

--- scripts/home.py
+++ scripts/home.py
@@ -4,11 +4,12 @@ from pyscript import when
 # Mostra dados do usuário armazenados no login
 usuario = window.sessionStorage.getItem("usuario")
 perfil = window.sessionStorage.getItem("perfil")
+nome = window.sessionStorage.getItem("nome")
 
 if not usuario or not perfil:
     window.location.href = "login.html"
 
-document.getElementById("user-nome").innerText = usuario
+document.getElementById("user-nome").innerText = nome
 document.getElementById("user-perfil").innerText = perfil.capitalize()
 
 @when("click", "#logout-btn")
--- scripts/login.py
+++ scripts/login.py
@@ -2,11 +2,11 @@ from js import document, window
 from pyscript import when
 
 usuarios = {
-    "medico": {"senha": "111", "perfil": "médico"},
-    "a": {"senha": "a", "perfil": "médico"},
-    "enfermeiro": {"senha": "111", "perfil": "enfermeiro"},
-    "paciente": {"senha": "111", "perfil": "paciente"},
-    "admin": {"senha": "111", "perfil": "administrador"},
+    "medico": {"senha": "111", "perfil": "médico", "nome": "Dra. Helena"},
+    "a": {"senha": "a", "perfil": "médico", "nome": "Dr. Xavier"},
+    "enfermeiro": {"senha": "111", "perfil": "enfermeiro", "nome": "Maria"},
+    "paciente": {"senha": "111", "perfil": "paciente", "nome": "Carlos"},
+    "admin": {"senha": "111", "perfil": "administrador", "nome": "Ana"},
 }
 
 @when("click", "#login-btn")
@@ -14,10 +14,13 @@ def fazer_login(event):
     usuario = document.getElementById("usuario").value
     senha = document.getElementById("senha").value
 
+    # Verifica se o usuário e senha estão corretos
     if usuario in usuarios and usuarios[usuario]["senha"] == senha:
         perfil = usuarios[usuario]["perfil"]
+        nome = usuarios[usuario]["nome"]
         window.sessionStorage.setItem("usuario", usuario)
         window.sessionStorage.setItem("perfil", perfil)
+        window.sessionStorage.setItem("nome", nome)
         window.location.href = "home.html"
         window.alert(f"Login realizado com sucesso como: {perfil}.")
     elif not usuario:
